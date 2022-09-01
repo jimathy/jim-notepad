8eb056bf9ec64586a48580e7ff573ceda65b874b
Jim Shield <gothicjim@gmail.com>
2022-09-01 20:17:08 +0100

    cn.lua locale added thanks to VossCat

---
+++ locales/cn.lua
@@ -0,0 +1,30 @@
+Loc["cn"] = {
+    error = {
+    
+    },
+    success = {
+    
+    },
+    info = {
+        
+    },
+    targetinfo = {
+        ["read"] = "阅读纸条",
+    },
+    text = {
+        ["enter_message"] = "在这里输入你的消息",
+    },
+    menu = {
+        ["make_a_note"] = "制作一个纸条",
+        ["message"] = "消息:",
+        ["written_by"] = "作者: ",
+        ["tear_up_note"] = "撕下纸条",
+    },
+    commands = {
+    
+    },
+    progressbar = {
+    
+    },
+    warning = {},
+}
