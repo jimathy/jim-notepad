50bccd3fe3fd4f702f5dfc72d38057a560b8a5f7
iplayer1337fivem <69944020+iplayer1337fivem@users.noreply.github.com>
2023-11-07 07:23:57 +0100

    Create da.lua
    danish translation

---
+++ locales/da.lua
@@ -0,0 +1,30 @@
+Loc["da"] = {
+    error = {
+    
+    },
+    success = {
+    
+    },
+    info = {
+        
+    },
+    targetinfo = {
+        ["read"] = "Læs note",
+    },
+    text = {
+        ["enter_message"] = "Indtast din besked her",
+    },
+    menu = {
+        ["make_a_note"] = "Lav en note",
+        ["message"] = "Besked:",
+        ["written_by"] = "Skrevet af: ",
+        ["tear_up_note"] = "Luk note",
+    },
+    commands = {
+    
+    },
+    progressbar = {
+    
+    },
+    warning = {},
+}
