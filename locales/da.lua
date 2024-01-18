1b99090e8b917a17a135a151f483d3571bcbf116
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-01-18 18:36:53 +0100

    Create da.lua
    danish lang

---
+++ locales/da.lua
@@ -0,0 +1,30 @@
+Loc["da"] = {
+    error = {
+    
+    },
+    success = {
+    
+    },
+    info = {
+        
+    },
+    targetinfo = {
+        ["read"] = "Læs memo",
+    },
+    text = {
+        ["enter_message"] = "Skriv din besked her",
+    },
+    menu = {
+        ["make_a_note"] = "Lav et memo",
+        ["message"] = "Besked:",
+        ["written_by"] = "Skrevet af: ",
+        ["tear_up_note"] = "Riv memo itu",
+    },
+    commands = {
+    
+    },
+    progressbar = {
+    
+    },
+    warning = {},
+}
